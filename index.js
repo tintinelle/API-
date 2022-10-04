c9de82db396b01efc8e5275088cbde530f3e38c1
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-10-05 00:50:58 +0500

    style css

--- index.js
+++ index.js
@@ -6,41 +6,26 @@ const activityAccessibility = document.getElementById('activityAccessibility');
 const activityPeople = document.getElementById('activityPeople');
 const activityPrice = document.getElementById('activityPrice');
 
-fetch('http://www.boredapi.com/api/activity/')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        console.log(data.activity);
-        console.log(data.type);
-        console.log(data.price);
-        console.log(data.participants);
-        console.log(data.accessibility);
 
-        activityName.innerHTML = data.activity;
-        activityType.innerHTML += data.type;
+document.getElementById('btnRandom').addEventListener('click', () => {
+    fetch('http://www.boredapi.com/api/activity/')
+        .then(response => response.json())
+        .then(data => {
 
-        (data.accessibility == 0) ? activityAccessibility.innerHTML += 'very easy':
-            (0 < data.accessibility <= 0.3) ? activityAccessibility.innerHTML += 'easy' :
-            (0.3 < data.accessibility <= 0.6) ? activityAccessibility.innerHTML += 'normal' :
-            (0.6 < data.accessibility <= 0.9) ? activityAccessibility.innerHTML += 'difficult' : activityAccessibility.innerHTML += 'very difficult';
+            activityName.innerHTML = data.activity;
+            activityType.innerHTML = `Activity type: ${data.type}`;
 
-        activityPeople.innerHTML += data.participants;
+            (data.accessibility == 0) ? activityAccessibility.innerHTML = 'Accessibility: very easy':
+                (0 < data.accessibility <= 0.3) ? activityAccessibility.innerHTML = 'Accessibility: easy' :
+                (0.3 < data.accessibility <= 0.6) ? activityAccessibility.innerHTML = 'Accessibility: normal' :
+                (0.6 < data.accessibility <= 0.9) ? activityAccessibility.innerHTML = 'Accessibility: difficult' : activityAccessibility.innerHTML = 'Accessibility: very difficult';
 
-        (data.price == 0) ? activityPrice.innerHTML += 'free':
-            (0 < data.price <= 0.3) ? activityPrice.innerHTML += 'minimum' :
-            (0.3 < data.price <= 0.6) ? activityPrice.innerHTML += 'medium' :
-            (0.6 < data.price <= 0.9) ? activityPrice.innerHTML += 'high' : activityPrice.innerHTML += 'maximum';
-    })
-    .catch(err => console.log(err));
+            activityPeople.innerHTML = `Number of participants: ${data.participants}`;
 
-// fetch('http://www.boredapi.com/documentation')
-// .then(response => response.json())
-// .then(data => {
-//     console.log(data); 
-//     // alert(data[0].question);
-// })
-// .catch(err => console.log(err));
-
-// if (data.price == 0) {
-//     activityPrice.innerHTML += `free`;
-// }
+            (data.price == 0) ? activityPrice.innerHTML = 'Price: free':
+                (0 < data.price <= 0.3) ? activityPrice.innerHTML = 'Price: minimum' :
+                (0.3 < data.price <= 0.6) ? activityPrice.innerHTML = 'Price: medium' :
+                (0.6 < data.price <= 0.9) ? activityPrice.innerHTML = 'Price: high' : activityPrice.innerHTML = 'Price: maximum';
+        })
+        .catch(err => console.log(err));
+});
